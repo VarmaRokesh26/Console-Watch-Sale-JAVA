b9660979d53d0d50bcf3b4fcadeee65261a8b0f6
VarmaScary123 <vrokeshvarma@gmail.com>
2024-02-19 11:55:36 +0530

    Start of Implementing DAO

---
+++ src/Model/User.java
@@ -0,0 +1,43 @@
+package Model;
+
+public class User {
+    private String userId;
+    private String userName;
+    private String mobileNumber;
+    private String emailId;
+    private String address;
+    private String password;
+
+    public User(String userId, String userName, String mobileNumber, String emailId, String address, String password) {
+        this.userId = userId;
+        this.userName = userName;
+        this.mobileNumber = mobileNumber;
+        this.emailId = emailId;
+        this.address = address;
+        this.password = password;
+    }
+    
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
